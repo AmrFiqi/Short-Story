bdd488ba59278a4465a88e905f40f8fc1c9ca69b
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-04-06 15:12:17 +0200

    Updated the code to be more scalable with more stories

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -11,7 +11,7 @@ import Foundation
 struct Story {
     var title: String
     var choice1: String
-    var choice2: String
     var choice1Destination: Int
-    var choice2Sestination: Int
+    var choice2: String
+    var choice2Destination: Int
 }
